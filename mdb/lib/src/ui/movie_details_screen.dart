1c962adfc6c8345084bf325dc464d8427235281b
alexandr.sushkov <[alexandr.sushkov.mail@gmail.com]>
2019-02-11 21:12:33 +0200

    details screen refactoring

--- mdb/lib/src/ui/movie_details_screen.dart
+++ mdb/lib/src/ui/movie_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:mdb/src/data/model/local/movie.dart';
 import 'package:mdb/src/utils/constants.dart';
 
@@ -11,6 +12,7 @@ class MovieDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    timeDilation = 10.0;
     return Scaffold(
         body: SingleChildScrollView(
       child: Column(
@@ -18,76 +20,24 @@ class MovieDetailsScreen extends StatelessWidget {
         children: <Widget>[
           Hero(
               tag: movie.id,
-              child: Container(
-                height: 300.0,
-                child: Image.network(
-                  '$imageBaseUrl$imageSizePrefixLarge${movie.poster_path}',
-                  fit: BoxFit.cover,
-                ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    height: 300.0,
+                    child: Image.network(
+                      '$imageBaseUrl$imageSizePrefixLarge${movie.poster_path}',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+
+                ],
               )),
-          Text(movie.title),
-          Text(movie.overview),
-          CastFilter(),
+          Hero(tag: 'title${movie.id}', child: Material(child: Text(movie.title))),
+          Material(child: Text(movie.overview)),
+
         ],
       ),
     ));
-  }
-}
-
-class ActorFilterEntry {
-  const ActorFilterEntry(this.name, this.initials);
-  final String name;
-  final String initials;
-}
-
-class CastFilter extends StatefulWidget {
-  @override
-  State createState() => CastFilterState();
-}
-
-class CastFilterState extends State<CastFilter> {
-  final List<ActorFilterEntry> _cast = <ActorFilterEntry>[
-    const ActorFilterEntry('Aaron Burr', 'AB'),
-    const ActorFilterEntry('Alexander Hamilton', 'AH'),
-    const ActorFilterEntry('Eliza Hamilton', 'EH'),
-    const ActorFilterEntry('James Madison', 'JM'),
-  ];
-  List<String> _filters = <String>[];
-
-  Iterable<Widget> get actorWidgets sync* {
-    for (ActorFilterEntry actor in _cast) {
-      yield Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: FilterChip(
-          avatar: CircleAvatar(child: Text(actor.initials)),
-          label: Text(actor.name),
-          selected: _filters.contains(actor.name),
-          onSelected: (bool value) {
-            setState(() {
-              if (value) {
-                _filters.add(actor.name);
-              } else {
-                _filters.removeWhere((String name) {
-                  return name == actor.name;
-                });
-              }
-            });
-          },
-        ),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Wrap(
-          children: actorWidgets.toList(),
-        ),
-        Text('Look for: ${_filters.join(', ')}'),
-      ],
-    );
   }
 }
